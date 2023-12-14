62738d32502af66c8296b22799d4a4d86f89e546
Ilky André <ilkyandrerl@gmail.com>
2023-12-14 14:22:04 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import boardgame.Position;
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Position pos = new Position(3, 9);
+        System.out.print(pos);
     }
 }
